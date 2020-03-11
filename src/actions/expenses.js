9f02592f717478ba6f42db1cf3547f831ebc4478
mayank <mayank.j@servify.tech>
2020-03-11 05:45:55 -0700

    fetching datat from database

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,11 +1,11 @@
 import uuid from 'uuid';
-import database from  '../firebase/firebase'
+import database from '../firebase/firebase';
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense
-})
+});
 
 export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
@@ -14,16 +14,17 @@ export const startAddExpense = (expenseData = {}) => {
       note = '',
       amount = 0,
       createdAt = 0
-    } = expenseData
-    const expense = { description, note, amount, createdAt }
-    database.ref().push(expense).then((ref) => {
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
-      }))
-    })
-  }
-}
+      }));
+    });
+  };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
@@ -37,3 +38,26 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
